3d600f3ec71623ba9dace2e9fcdafff7bdbf8813
Naoki Sekiguchi <naokis@ranagram.com>
2013-03-06 17:23:55 +0900

    Created tentatively working version

--- js/modal.js
+++ js/modal.js
@@ -11,229 +11,222 @@
 
 $.Modal = Backbone.View.extend({
 	options: {
-//        slideEl: '.slide',
 //        animateDuration: 750,
 //        animateEasing: 'swing',
-		width: null,
+//        width: null,
 		cache: true,
 		boxClassName: 'modal-box',
 		boxBodyClassName: 'modal-body',
 		bgClassName: 'modal-bg',
 		innerLinkSelector: null,
-		dismissEl: '.dismiss',
+		dismissClassName: 'dismiss',
 		bodyEl: document.body,
 		action: { }
 	},
-	events: {
-//        'click': '_openModal',
-//        'click .prev': '_scrollPrev',
-//        'click .controll a': '_jump'
-	},
 	initialize: function (options) {
+		/**
+		 * <div $body>
+		 *   <div $box>
+		 *     <div $boxBody> </div>
+		 *   </div>
+		 *   <div $bg> </div>
+		 *   <button $dismiss></button>
+		 * </div>
+		 */
 		var opt = this.options;
 		var id, url;
 		_.extend(this.options, options);
 		_.extend(this.action, opt.action);
-
-		// element
-		// - bg
-		if (typeof $('.' + opt.bgClassName)[0] != 'object') {
+		// element - bg
+//        this.$bg = $('.' + opt.bgClassName);
+//        if (typeof this.$bg[0] != 'object') {
 			this.$bg = new Backbone.View({
-				className: this.options.bgClassName
+				className: opt.bgClassName
 			}).$el;
-		}
-		// - box
+			this.$bg.appendTo(document.body);
+//        }
+		// element - box
 		id = this.$el.data('modal-id');
 		url = this.$el.data('modal-url');
 		this.$box;
 
 		if (typeof id === 'string') { // id mode
-			this.$box = $('#' + id);
+			this.$box = $('#' + id).hide();
+			this.$boxBody = this.$box.find('.' + opt.boxBodyClassName);
+			this.$dismiss = this.$box.find('.' + opt.dismissClassName);
 		} else if (!this.$box && typeof url === 'string') { // url mode
 			this.$box = new Backbone.View({
-				className: this.options.boxClassName
+				className: opt.boxClassName
 			}).$el;
-			this.$box.appendTo(document.body);
+			this.url = this.$el.data('modal-url'); 
+			this.$boxBody = $('<div/>', { 'class': opt.boxBodyClassName }).appendTo(this.$box);
+			this.$dismiss = $('<a/>', { 'class': opt.dismissClassName, href: '#', html: '&#215;' }).appendTo(this.$box);
+			this.$box.hide().appendTo(document.body);
 		}
-		this.$boxBody = $('<div/>', { 'class': opt.boxBodyClassName });
+		this.$body = $(opt.bodyEl);
 
-		/*
-		if (!this.container[0]) {
-			this.container = $('<div/>', {
-					'class': this.containerClassName
-				}).hide().appendTo(document.body);
-			this.inner = $('<div/>', {
-					'class': this.containerClassName + '_inner'
-				}).appendTo(this.container);
-		}
-		this.body = $(this.bodyEl);
-		this.closeBtn = $(this.dismissEl);
-
-		this.isIE7 = ($.browser.msie && $.browser.version < 8);
-		var url = this.$el.attr('href');
-
-		// bind event
-		this.$el.bind('click', $.proxy(function (e) {
-			this._openModal(url);
-			$(document).bind('keydown.smb', $.proxy(this._keyEventHandler, this));
-			e.preventDefault();
-		}, this));
-
-		// property
-		this.action.initComplete.call(this);
-		*/
-
+		_.bindAll(this);
+		this._setupEvents();
+		this.action.initComplete.call(this); // action
 		this.render();
 	},
 	render: function () {
-		this.action.renderComplete.call(this);
+		this.action.renderComplete.call(this); // action
+	},
+	events: {
+		'click.modal': '_open'
+	},
+	_setupEvents: function () {
+		var opt = this.options;
+		$(document).on('keydown.modal', this._keyHandler);
+		$(document).on('click.modal', '.' + opt.dismissClassName, this._close);
+		$(document).on('click.modal', '.' + opt.bgClassName, this._close);
+//        $(document).on('click.modal', opt.innerLinkSelector, this._openInside);
 	},
 	// close modal with Esc key
-	_keyEventHandler: function (e) {
+	_keyHandler: function (e) {
 		var key = e.keyCode || e.charCode;
-		if (key == 27) {
-			this._closeModal();
-			$(document).unbind('keydown.smb');
+		if (key === 27) {
+			this._close();
 		}
 	},
-
-	_bindContentEvents: function () {
-		var self = this;
-		// close button
-		$(this.dismissEl).bind('click', function (e) {
-			self._closeModal();
-			$(document).unbind('keydown.smb');
-			e.preventDefault();
-		});
-		// bg
-		this.$bg.unbind('click').bind('click', function (e) {
-			self._closeModal();
-			$(document).unbind('keydown.smb');
-			e.preventDefault();
-		});
-		// inner link
-		if (this.innerLinkSelector) {
-			$(this.innerLinkSelector).bind('click', function (e) {
-				var href = $(this).attr('href');
-				if (!href) { return; }
-				self._openInside(href);
-				e.preventDefault();
-			});
-		}
-	},
-
-	_openModal: function (url) {
-		var cacheCtrl = this.cache ? '' : '?d=' + (new Date()).getTime();
-		if (this.width) {
-			this.container.css({
-				'width': this.width,
-				'margin-left': Math.floor(this.width / 2) * -1
+	_open: function (e) {
+		var opt = this.options;
+//        if (opt.width) {
+//            this.$box.css({
+//                'width': opt.width,
+//                'margin-left': Math.floor(opt.width / 2) * -1
+//            });
+//        } else {
+//            this.$box.css({
+//                'width': '',
+//                'margin-left': ''
+//            });
+//        }
+		if (typeof this.url === 'string') {
+			$.ajax(this.url, {
+				cache: opt.cache,
+				dataType: 'html',
+				success: _.bind(fire, this)
 			});
 		} else {
-			this.container.css({
-				'width': '',
-				'margin-left': ''
-			});
+			fire.call(this);
 		}
-		this.inner.load(url + cacheCtrl, $.proxy(function() {
-			this._initContainer();
+
+		function fire (res) {
+			if (res) {
+				var body = res.slice(res.search(/<body/), res.search(/<\/body>/));
+				body = body.replace(/<body[^>]*>\n?/, '');
+				this.$boxBody.html(body);
+			}
+			this._initBox();
 			this._showBg();
-			this._showContainer();
-			this._bindContentEvents();
-		}, this));
-	},
-
-	_closeModal: function () {
-		this.inner.empty();
-		this.container.hide();
-		$(window).scrollTop(this.initialScrollTop);
-		if (this.isIE7) {
-			this.$bg.hide();
-			this.closeCallback();
-		} else {
-			this.$bg.fadeOut($.proxy(function() {
-				this.closeCallback();
-			}, this));
+			this._showBoxBody();
+			this.action.openComplete.call(this); // action
 		}
+		e.preventDefault();
 	},
-
-	_initContainer: function () {
-		var winHeight = $(window).height(),
-			scrollTop = $(window).scrollTop();
-		if (winHeight < scrollTop
-				&& this.body.outerHeight() < scrollTop) {
+	_close: function (e) {
+		/**
+		 * <div $body>
+		 *   <div $box>
+		 *     <div $boxBody> </div>
+		 *   </div>
+		 *   <div $bg> </div>
+		 *   <button $dismiss></button>
+		 * </div>
+		 */
+		var opt = this.options;
+		this.$boxBody.hide();
+		this.$box.hide();
+		$(window).scrollTop(this.initialScrollTop);
+//        if (this.isIE7) {
+//            this.$bg.hide();
+//            this.closeCallback();
+//        } else {
+			this.$bg.fadeOut(_.bind(this.action.closeComplete, this)/* action */);
+//        }
+	},
+	_initBox: function () {
+		var winH = $(window).height(),
+			bodyH = this.$body.outerHeight(),
+			boxH = this.$box.outerHeight(), 
+			scrollTop = $(window).scrollTop(),
+			posTop;
+		if (winH < scrollTop && bodyH < scrollTop) {
 			// prevent too much increase of height
 		} else {
+			// save scrll position
 			this.initialScrollTop = scrollTop;
 		}
 
-		var posTop = (this.container.outerHeight() < winHeight) ?
-			this.initialScrollTop + Math.floor((winHeight - this.container.outerHeight()) / 2) :
-			this.initialScrollTop;
-
-		this.container.css({
+		if (this.$box.outerHeight() < winH) {
+			// small box height
+			posTop = this.initialScrollTop + Math.floor((winH - boxH) / 2);
+		} else {
+			// large box height
+			posTop = this.initialScrollTop;
+		}
+		this.$box.css({
 			'top': posTop
 		}).show();
-		this.inner.css({
-			'visibility': 'hidden'
-		});
+		this.$boxBody.hide();
+//        this.$boxBody.css({
+//            'visibility': 'hidden'
+//        });
 	},
-
-	_showContainer: function () {
-		this.inner.css({
-			'visibility': 'visible'
-		});
+	_showBoxBody: function () {
+		this.$boxBody.fadeIn();
+//        this.$boxBody.css({
+//            'visibility': 'visible'
+//        });
 	},
-
 	_showBg: function () {
 		this._adjustBgSize();
-		if (this.isIE7) {
-			this.$bg.show();
-		} else {
+//        if (this.isIE7) {
+//            this.$bg.show();
+//        } else {
 			this.$bg.fadeIn();
-		}
+//        }
 	},
-
 	_adjustBgSize: function () {
 		this.$bg.width(this._getBgWidth()).height(this._getBgHeight());
 	},
-
 	_getBgWidth: function () {
-		var winWidth = $(window).width(),
-			bodyWidth = this.body.outerWidth(),
-			contentWidth = this.container.outerWidth();
-		return _.max([winWidth, bodyWidth, contentWidth]);
+		var winW = $(window).width(),
+			bodyW = this.$body.outerWidth(),
+			contentW = this.$box.outerWidth();
+		return _.max([winW, bodyW, contentW]);
 	},
-
 	_getBgHeight: function () {
-		var winHeight = $(window).height(),
-			bodyHeight = this.body.outerHeight(),
-			contentHeight = this.container.outerHeight() + $(window).scrollTop();
-		return _.max([winHeight, bodyHeight, contentHeight]);
+		var winH = $(window).height(),
+			bodyH = this.$body.outerHeight(),
+			contentH = this.$box.outerHeight() + $(window).scrollTop();
+		return _.max([winH, bodyH, contentH]);
 	},
-
-	_openInside: function (url) {
-		var cacheCtrl = this.cache ? '' : '?d=' + (new Date()).getTime();
-		this.inner.load(url + cacheCtrl, $.proxy(function() {
-			// for fadeIn effect
-			this.container.height(this.container.height());
-			this.inner.hide();
-
-			this._adjustBgSize();
-			this._initContainer();
-			$(window).scrollTop(this.initialScrollTop);
-			this._showContainer();
-			this._bindContentEvents();
-
-			// fadeIn effect
-			if (this.isIE7) {
-				this.inner.show();
-			} else {
-				this.inner.fadeIn();
-			}
-			this.container.css('height', '');
-		}, this));
-	},
+	_openInside: function (e) {//{{{
+		var url = e.currentTarget;
+		var opt = this.options;
+		$.ajax(url, {
+			cache: opt.cache,
+			success: _.bind(function () {
+				this.$box.height(this.$box.height()); // fix height
+				this.$boxBody.hide();
+				this._adjustBgSize();
+				this._initBox();
+				$(window).scrollTop(this.initialScrollTop);
+				this._showBoxBody();
+				// fadeIn effect
+//                if (this.isIE7) {
+//                    this.inner.show();
+//                } else {
+					this.$boxBody.fadeIn();
+//                }
+				this.$box.css('height', ''); // reset height
+			}, this)
+		});
+		e.preventDefault();
+	},//}}}
 
 	// interface functions that should be overridden
 	action: {
