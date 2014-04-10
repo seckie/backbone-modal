17df9d9b8fdd63090649813ddfa39fa95b216902
Naoki Sekiguchi <naokis@ranagram.com>
2014-04-10 17:54:36 +0900

    Added settings for CoffeeScript

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,84 +1,97 @@
 module.exports = function (grunt) {
-	grunt.loadNpmTasks('grunt-contrib-compass');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-rename');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-rename');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		compass: {
-			options: {
-				httpPath: '/backbone-modal/',
-				sassDir: '_scss',
-				cssDir: 'css',
-				imagesDir: 'img',
-				relativeAssets: true
-			},
-			dev: {
-				options: {
-					environment: "development",
-					outputStyle: 'compact',
-					noLineComments: true,
-					assetCacheBuster: false
-				}
-			}
-		},
-		jshint: {
-			main: [ 'modal.js' ]
-		},
-		copy: {
-			main: {
-				files: [
-					{
-						expand: true,
-						cwd: 'bower_components/jquery/dist/',
-						src: [ 'jquery.min.js' ],
-						dest: 'js/'
-					},
-					{
-						expand: true,
-						cwd: 'bower_components/jquery-1.11.0/',
-						src: [ 'index.js' ],
-						dest: 'js/'
-					},
-					{
-						expand: true,
-						cwd: 'bower_components/underscore/',
-						src: [ 'underscore.js' ],
-						dest: 'js/'
-					},
-					{
-						expand: true,
-						cwd: 'bower_components/backbone/',
-						src: [ 'backbone.js' ],
-						dest: 'js/'
-					}
-				]
-			}
-		},
-		rename: {
-			main: {
-				files: [
-					{
-						src: [ 'js/index.js' ],
-						dest: 'js/jquery-1.11.0.min.js'
-					}
-				]
-			}
-		},
-		watch: {
-			scss: {
-				files: [ '_scss/*.scss' ],
-				tasks: [ 'compass:dev' ]
-			},
-			js: {
-				files: [ 'modal.js' ],
-				tasks: [ 'jshint' ]
-			}
-		}
-	});
+  grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      coffee: {
+        main: {
+          options: {},
+          files: {
+            'modal.js': '_coffee/*.coffee'
+          }
+        }
+      },
+      compass: {
+        options: {
+          httpPath: '/backbone-modal/',
+          sassDir: '_scss',
+          cssDir: 'css',
+          imagesDir: 'img',
+          relativeAssets: true
+        },
+        dev: {
+          options: {
+            environment: "development",
+            outputStyle: 'compact',
+            noLineComments: true,
+            assetCacheBuster: false
+          }
+        }
+      },
+      jshint: {
+        main: [ 'modal.js' ]
+      },
+      copy: {
+        main: {
+          files: [
+            {
+              expand: true,
+              cwd: 'bower_components/jquery/dist/',
+              src: [ 'jquery.min.js' ],
+              dest: 'js/'
+            },
+            {
+              expand: true,
+              cwd: 'bower_components/jquery-1.11.0/',
+              src: [ 'index.js' ],
+              dest: 'js/'
+            },
+            {
+              expand: true,
+              cwd: 'bower_components/underscore/',
+              src: [ 'underscore.js' ],
+              dest: 'js/'
+            },
+            {
+              expand: true,
+              cwd: 'bower_components/backbone/',
+              src: [ 'backbone.js' ],
+              dest: 'js/'
+            }
+          ]
+        }
+      },
+      rename: {
+        main: {
+          files: [
+            {
+              src: [ 'js/index.js' ],
+              dest: 'js/jquery-1.11.0.min.js'
+            }
+          ]
+        }
+      },
+      watch: {
+        coffee: {
+          files: [ '_coffee/*.coffee' ],
+          tasks: [ 'coffee' ]
+        },
+        scss: {
+          files: [ '_scss/*.scss' ],
+          tasks: [ 'compass:dev' ]
+        },
+        js: {
+          files: [ 'modal.js' ],
+          tasks: [ 'jshint' ]
+        }
+      }
+  });
 
-	grunt.registerTask('default', [ 'compass:dev', 'watch' ]);
-	grunt.registerTask('deploy', [ 'copy', 'rename', 'compass:dev' ]);
+  grunt.registerTask('default', [ 'coffee', 'compass:dev', 'watch' ]);
+  grunt.registerTask('deploy', [ 'copy', 'rename', 'coffee', 'compass:dev' ]);
 };
